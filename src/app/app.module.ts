7a3749dc69d81f2e53768b1b4594dd7d545982fd
NajeebArif <NajeebArif786@outlook.com>
2018-11-06 02:51:07 +0530

    Adding vanilla ng new app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { DateComponent } from './date/date.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloWorldComponent,
-    DateComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
