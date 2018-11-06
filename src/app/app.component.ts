49daf737dceaf78abfde0714b2fa96df25e89a60
NajeebArif <NajeebArif786@outlook.com>
2018-11-06 12:08:32 +0530

    Adding Services and making rest calls using angular/common/http pkg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TestService } from './test.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  constructor(private svc:TestService){
+  constructor(private svc:TestService, private http:HttpClient){
     svc.printToConsole("Got the service!!!")
   }
   
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+   // let obs = this.http.get('https://api.github.com/users/najeebarif');
+    //obs.subscribe((response)=>console.log(response));
+  }
 }
--- src/app/view/view/view.component.ts
+++ src/app/view/view/view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-view',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
+  userName: string;
+  resp: Object;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+  }
+
+  search(){
+    this.http.get('https://api.github.com/users/'+this.userName)
+    .subscribe((resp)=>{
+      this.resp = resp;
+      console.log(this.resp);
+    })
   }
 
 }
